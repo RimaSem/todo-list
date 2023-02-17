44a8b17aac04d5e920b3a52a46afa9af42ac150c
RimaSem <rima.semetaite@gmail.com>
2023-02-17 16:28:12 +0200

    Add animated hamburger menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,24 @@
+import { useRef } from "react";
+
 function Menu() {
-  return <div className="menu"></div>;
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  function displayMenu() {
+    menuRef.current && menuRef.current.classList.toggle("active");
+  }
+
+  return (
+    <>
+      <div className="off-screen-menu"></div>
+      <div className="menu">
+        <div ref={menuRef} className="hamburger-menu" onClick={displayMenu}>
+          <div className="ham-bar bar-top"></div>
+          <div className="ham-bar bar-mid"></div>
+          <div className="ham-bar bar-bottom"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Menu;
