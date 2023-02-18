0514b811cca21811b2099e6fb1e514b954bdb986
RimaSem <rima.semetaite@gmail.com>
2023-02-18 15:23:36 +0200

    Add content buttons

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,17 @@
+import Icon from "@mdi/react";
+import { mdiPlus } from "@mdi/js";
+
 function Content() {
-  return <div className="content"></div>;
+  return (
+    <div className="content">
+      <div className="content-btns">
+        <button className="sort-btn">Important</button>
+        <button className="new-task-btn">
+          <Icon path={mdiPlus} size={0.8} /> Create Todo
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
